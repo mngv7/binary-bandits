b98e2a59671b0471c278e89a649b847dc220dae7
mngv7 <zackariya.taylor@gmail.com>
2024-08-20 14:12:08 +1000

    Added an initial design for the login page.

--- src/main/java/com/example/protrack/HelloApplication.java
+++ src/main/java/com/example/protrack/HelloApplication.java
@@ -12,8 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello, my name is Zack!");
-        stage.setTitle("Hello Binary Bandits!");
+        stage.setTitle("ProTrack");
         stage.setScene(scene);
         stage.show();
     }
