824d9e10beb0b5d671794d4755311735e858d416
mngv7 <zackariya.taylor@gmail.com>
2024-08-25 10:52:19 +1000

    Created a database connection and added the .db file to .gitignore. For future reference, do not track the database files.

--- src/main/java/com/example/protrack/Main.java
+++ src/main/java/com/example/protrack/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.sql.Connection;
 
 import java.io.IOException;
 
@@ -20,6 +21,8 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
+        Connection connection = DatabaseConnection.getInstance();
+
         launch();
     }
 }
