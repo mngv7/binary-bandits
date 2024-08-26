cc8c8b7a47c78e8843f043de61dc3a2dddd672e8
mngv7 <zackariya.taylor@gmail.com>
2024-08-26 12:17:36 +1000

    Added a function to check if the username and password field were blank. Added the db-journal to the .gitignore file.

--- src/main/java/com/example/protrack/applicationpages/LoginPageController.java
+++ src/main/java/com/example/protrack/applicationpages/LoginPageController.java
@@ -6,19 +6,40 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class LoginPageController {
+
+    @FXML
+    private PasswordField passwordTextField;
+
+    @FXML
+    private TextField usernameTextField;
+
     @FXML
     private Button loginButton;
 
     @FXML
     protected void onLoginButtonClick() throws IOException {
+        if (isInputValid()) {
+            loadHomePage();
+        }
+    }
+
+    private void loadHomePage() throws IOException {
         Stage stage = (Stage) loginButton.getScene().getWindow();
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("home-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), Main.WIDTH, Main.HEIGHT);
         stage.setScene(scene);
+    }
+
+    private boolean isInputValid() {
+        String username = usernameTextField.getText();
+        String password = passwordTextField.getText();
+        return !username.trim().isEmpty() && !password.trim().isEmpty();
     }
 }
