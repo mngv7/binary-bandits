ff5c3285f6c5af1f96dc434620177cfdc31de79a
mngv7 <zackariya.taylor@gmail.com>
2024-08-26 16:47:36 +1000

    Fixed accesslevel instantiaters for the user classes.

--- src/main/java/com/example/protrack/users/ProductionUser.java
+++ src/main/java/com/example/protrack/users/ProductionUser.java
@@ -1,12 +1,15 @@
 package com.example.protrack.users;
 
 public class ProductionUser extends AbstractUser {
+    private final String accessLevel;
+
     public ProductionUser(int employeeId, String firstName, String lastName, String password) {
         super(employeeId, firstName, lastName, password);
+        this.accessLevel = "LOW";
     }
 
     @Override
     public String getAccessLevel() {
-        return "Medium";
+        return accessLevel;
     }
 }
--- src/main/java/com/example/protrack/users/WarehouseUser.java
+++ src/main/java/com/example/protrack/users/WarehouseUser.java
@@ -1,12 +1,15 @@
 package com.example.protrack.users;
 
 public class WarehouseUser extends AbstractUser {
+    private final String accessLevel;
+
     public WarehouseUser(int employeeId, String firstName, String lastName, String password) {
         super(employeeId, firstName, lastName, password);
+        this.accessLevel = "MEDIUM";
     }
 
     @Override
     public String getAccessLevel() {
-        return "Low";
+        return accessLevel;
     }
 }
