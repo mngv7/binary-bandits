7ad51c155889a299c3a45bacc641207dabb5137e
seahauk <n11945427@qut.edu.au>
2024-09-02 13:32:14 +1000

    fix: added org.example export in module-info.java - allows javafx.graphics to access MainApp class

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,17 @@
 module com.example.protrack {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
     requires java.sql;
     requires jbcrypt;
 
 
     opens com.example.protrack to javafx.fxml;
+    exports org.example;
     exports com.example.protrack;
     exports com.example.protrack.applicationpages;
     opens com.example.protrack.applicationpages to javafx.fxml;
     exports com.example.protrack.databaseutil;
     opens com.example.protrack.databaseutil to javafx.fxml;
+    opens org.example to javafx.fxml;
 }
