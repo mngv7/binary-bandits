b842a883963484e2cfde81fdc53291445260a9ce
mngv7 <zackariya.taylor@gmail.com>
2024-09-08 18:48:50 +1000

    Rebased with master.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,9 +9,13 @@ module com.example.protrack {
 
     opens com.example.protrack to javafx.fxml;
     opens com.example.protrack.products to javafx.base;
+    opens com.example.protrack.parts to javafx.base;  // From HEAD
+    opens com.example.protrack.database to javafx.base;  // From Got the price column in products page working
 
     exports com.example.protrack;
     exports com.example.protrack.products;
+    exports com.example.protrack.parts;  // From HEAD
+    exports com.example.protrack.database;  // From Got the price column in products page working
     exports com.example.protrack.users;
     exports com.example.protrack.applicationpages;
     opens com.example.protrack.applicationpages to javafx.fxml;
