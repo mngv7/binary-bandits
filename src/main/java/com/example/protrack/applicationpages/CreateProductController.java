c9712821650ad320909808b8d612230263df8d23
Lucas <n11273321@qut.edu.au>
2024-09-08 17:18:24 +1000

    Got the button to add the product to the db, but reqparts are still having issues.

--- src/main/java/com/example/protrack/applicationpages/CreateProductController.java
+++ src/main/java/com/example/protrack/applicationpages/CreateProductController.java
@@ -1,5 +1,7 @@
 package com.example.protrack.applicationpages;
 
+import com.example.protrack.database.ProductDBTable;
+import com.example.protrack.databaseutil.DatabaseConnection;
 import com.example.protrack.products.Product;
 import com.example.protrack.products.ProductDAO;
 import com.example.protrack.products.RequiredParts;
@@ -9,19 +11,23 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.BooleanBinding;
+import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.*;
 
 public class CreateProductController {
     private static final String TITLE = "Create Product";
     private static final int WIDTH = 900;
     private static final int HEIGHT = 360;
 
+    /*
     @FXML
     public TextField partId1;
 
@@ -39,6 +45,14 @@ public class CreateProductController {
 
     @FXML
     public TextField requireAmount3;
+    */
+
+    @FXML
+    private TextField partIdSearchField;
+
+    @FXML
+    private VBox partResultVBox;
+
 
     @FXML
     public Button createProductButton;
@@ -51,6 +65,7 @@ public class CreateProductController {
 
     public void initialize() {
         // Create a binding to check if any field is empty
+        /*
         BooleanBinding fieldsEmpty = Bindings.createBooleanBinding(() ->
                         productIdField.getText().trim().isEmpty() ||
                                 productNameField.getText().trim().isEmpty() ||
@@ -69,7 +84,49 @@ public class CreateProductController {
                 partId3.textProperty(),
                 requireAmount3.textProperty()
         );
-        createProductButton.disableProperty().bind(fieldsEmpty);
+        */
+
+        //createProductButton.disableProperty().bind(fieldsEmpty);
+    }
+
+    @FXML
+    protected void partSearch() {
+        Connection connection;
+        connection = DatabaseConnection.getInstance();
+
+        String partIdStr = partIdSearchField.getText();
+
+        if (partIdStr != null && !partIdStr.trim().isEmpty()) {
+            // Clear previous results
+            // partResultVBox.getChildren().clear();
+
+            int partIdInt = Integer.parseInt(partIdStr);
+            try {
+                PreparedStatement getPartId = connection.prepareStatement(
+                        "SELECT * " +
+                                "FROM parts a " +
+                                "WHERE a.partsId = ?");
+                getPartId.setInt(1, partIdInt);
+                ResultSet rs = getPartId.executeQuery();
+
+                if (rs.next()) {
+                    //Make the new vbox with those partid
+                    // Create a new row with the productID and a TextField
+                    VBox newRow = new VBox();
+                    Label idLabel = new Label("Part ID: " + rs.getString("partsId"));
+                    Label idLabel2 = new Label("Part Name: " + rs.getString("name"));
+                    TextField textField = new TextField();
+                    newRow.getChildren().addAll(idLabel, idLabel2, textField);
+
+                    // Add the new row to the result VBox
+                    partResultVBox.getChildren().add(newRow);
+                }
+
+            } catch(SQLException ex) {
+                System.err.println(ex);
+
+            }
+        }
     }
 
     /*TODO
@@ -81,7 +138,7 @@ public class CreateProductController {
     1. Make it so that you can search for Parts.
     2. Generate new row for said part, where you can add number of parts
     3. Ensure that when create product is pressed, it generates a new input in product table
-    4. "" For requiredParts table
+    4. "" For requiredParts table using the parts generated in (2)
     5. Open test records table.
      */
 
@@ -102,8 +159,12 @@ public class CreateProductController {
         ProductDAO productDAO = new ProductDAO();
         RequiredPartsDAO requiredPartsDAO = new RequiredPartsDAO();
 
+        int rowCount = partResultVBox.getChildren().size();
+        System.out.println("Number of rows: " + rowCount);
+
         try {
             int productId = Integer.parseInt(productIdField.getText());
+
             String productName = productNameField.getText();
 
             long millis = System.currentTimeMillis();
@@ -111,6 +172,9 @@ public class CreateProductController {
 
             productDAO.newProduct(new Product(productId, productName, date));
 
+            insertProductsIntoDB(productId, productName, date);
+
+            /*
             requiredPartsDAO.newRequiredParts(new RequiredParts(Integer.parseInt(partId1.getText()), productId, Integer.parseInt(requireAmount1.getText())));
             requiredPartsDAO.newRequiredParts(new RequiredParts(Integer.parseInt(partId2.getText()), productId, Integer.parseInt(requireAmount2.getText())));
             requiredPartsDAO.newRequiredParts(new RequiredParts(Integer.parseInt(partId3.getText()), productId, Integer.parseInt(requireAmount3.getText())));
@@ -123,8 +187,66 @@ public class CreateProductController {
             productIdField.setText("");
             productNameField.setText("");
             openCreateTestRecordPopup();
+            */
+
+            //insertReqPartsFromVbox(productId);
+
         } catch (NumberFormatException e) {
             System.out.println("Invalid product ID. Please enter a valid number.");
+        }
+    }
+
+    private void insertProductsIntoDB(int productId, String productName, java.sql.Date date) {
+        Connection connection;
+        connection = DatabaseConnection.getInstance();
+        try {
+            PreparedStatement insertProduct = connection.prepareStatement(
+                    "INSERT INTO products (productId, productName, dateCreated ) " +
+                            "VALUES (?, ?, ?)");
+            insertProduct.setInt(1, productId);
+            insertProduct.setString(2, productName);
+            insertProduct.setDate(3, date);
+
+            System.out.println("productId" + productId + " productName " + productName + " date " + date);
+
+        } catch (SQLException ex) {
+            System.err.println(ex);
+        }
+    }
+
+    private void insertReqPartsFromVbox(int productId) {
+        Connection connection;
+        connection = DatabaseConnection.getInstance();
+
+        System.out.println("HERE!");
+        try {
+            connection.setAutoCommit(false); // Use transaction to handle multiple inserts
+            PreparedStatement insertParts = connection.prepareStatement(
+                    "INSERT INTO requiredParts (partsId, productId, requiredAmount) " +
+                            "VALUES (?, ?, ?)");
+            for (var node : partResultVBox.getChildren()) {
+                if (node instanceof  VBox) {
+                    VBox row = (VBox) node;
+
+                    Label idLabel = (Label) row.getChildren().get(0);
+                    String partsId = idLabel.getText().replace("Parts ID: ", "");
+
+                    TextField amountField = (TextField) row.getChildren().get(1);
+                    String requiredAmount = amountField.getText();
+
+                    insertParts.setInt(1, Integer.parseInt(partsId));
+                    insertParts.setInt(2, productId);
+                    insertParts.setInt(3, Integer.parseInt(requiredAmount));
+                    System.out.println("PartsId" + partsId + " ProductId " + productId + " Amount " + requiredAmount);
+                    insertParts.addBatch();
+                }
+            }
+            insertParts.executeBatch();
+            connection.commit();
+
+
+        } catch (SQLException ex) {
+            System.err.println(ex);
         }
     }
 
