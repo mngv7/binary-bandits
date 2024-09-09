5ac78c66f7a57ea93f28bed6d147dbc4c6e90b18
seahauk <n11945427@qut.edu.au>
2024-09-09 17:14:55 +1000

    Specialised profile into separate controllers with minor UI additions to the profile & employee pages.

---
+++ src/main/java/com/example/protrack/applicationpages/ProfileController.java
@@ -0,0 +1,20 @@
+package com.example.protrack.applicationpages;
+
+
+import javafx.fxml.FXML;
+
+import com.example.protrack.databaseutil.DatabaseConnection;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ProfileController {
+
+    public ProfileController() { }
+
+}
