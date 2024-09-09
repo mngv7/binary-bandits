1d520d7d7eb46a12f4ef102c516fed47e51061c5
seahauk <n11945427@qut.edu.au>
2024-09-09 21:31:27 +1000

    Created Customer Class with constructor, getters, and setters

--- src/main/java/com/example/protrack/profile/ProfileWorkOrdersController.java
+++ src/main/java/com/example/protrack/profile/ProfileWorkOrdersController.java
@@ -1,25 +1,22 @@
     package com.example.protrack.profile;
 
-import com.example.protrack.applicationpages.Product;
-import com.example.protrack.databaseutil.DatabaseConnection;
+import com.example.protrack.workorder.WorkOrder;
+import com.example.protrack.workorder.WorkOrdersDAO;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ProfileWorkOrdersController {
 
-    Connection connection = DatabaseConnection.getInstance();
+    private ListView pendingOrdersListView;
 
-
+    private WorkOrdersDAO workOrdersDAO;
 
     @FXML
     private void displayPendingWorkOrders() {
-
+        HashMap<Integer, WorkOrder> pendingWorkOrders = workOrdersDAO.getWorkOrderByStatus();
+        pendingOrdersListView.getItems().setAll(pendingWorkOrders);
     }
 
     private void acceptWorkOrder() {
