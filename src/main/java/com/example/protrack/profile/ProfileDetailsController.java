fe4060e1d5ae95ff5575a4322753a020b5316d0a
seahauk <n11945427@qut.edu.au>
2024-09-09 18:05:42 +1000

    fix: UI was errant due to not having the correct css stylesheet for relevant pages. modified the loadContent function in MainController to update required stylesheets

---
+++ src/main/java/com/example/protrack/profile/ProfileDetailsController.java
@@ -0,0 +1,15 @@
+package com.example.protrack.profile;
+
+import javafx.fxml.FXML;
+
+public class ProfileDetailsController {
+
+    private void loadPersonalDetails() {
+
+    }
+
+    @FXML
+    private void editPersonalDetails() {
+
+    }
+}
