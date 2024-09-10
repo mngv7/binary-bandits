bae806d77248f3303e834b777fd1546d4b73eabb
seahauk <n11945427@qut.edu.au>
2024-09-10 19:58:31 +1000

    fix: many DAO functions were not returning correctly and is now fixed. Pending work orders are now displayed on customer profile

--- src/main/java/com/example/protrack/customer/CustomerDAO.java
+++ src/main/java/com/example/protrack/customer/CustomerDAO.java
@@ -90,11 +90,11 @@ public class CustomerDAO {
             while (rs.next()) {
                 Customer customer = mapResultSetToCustomer(rs);
                 customers.put(customer.getCustomerId(), customer);
+                System.out.println(customer.getCustomerId());
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
         return customers;
     }
 
