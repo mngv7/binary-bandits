ee2c8a75c65e1495363e3492f6657d37065106d9
seahauk <n11945427@qut.edu.au>
2024-09-10 19:58:31 +1000

    fix: many DAO functions were not returning correctly and is now fixed. Pending work orders are now displayed on customer profile

--- src/main/java/com/example/protrack/workorder/WorkOrdersDAOImplementation.java
+++ src/main/java/com/example/protrack/workorder/WorkOrdersDAOImplementation.java
@@ -33,12 +33,12 @@ public class WorkOrdersDAOImplementation implements WorkOrdersDAO.WorkOrdersDAOI
             createTable.execute(
                     "CREATE TABLE IF NOT EXISTS work_orders ("
                             + "work_order_id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                            + "work_order_owner VARCHAR, "
-                            + "customer_name VARCHAR NOT NULL, "
+                            + "work_order_owner_id INTEGER, "
+                            + "customer_id INTEGER NOT NULL, "
                             + "order_date DATETIME NOT NULL, "
                             + "delivery_date DATETIME, "
                             + "shipping_address VARCHAR NOT NULL, "
-                            + "products_id INTEGER NOT NULL , "
+                            + "products_id INTEGER , "
                             + "status VARCHAR NOT NULL , "
                             + "subtotal DOUBLE NOT NULL "
                             + ")"
@@ -48,8 +48,19 @@ public class WorkOrdersDAOImplementation implements WorkOrdersDAO.WorkOrdersDAOI
         }
     }
 
+    public void dropTable() {
+        String query = "DROP TABLE IF EXISTS work_orders";
+
+        try (Statement stmt = connection.createStatement()) {
+            stmt.execute(query);
+            System.out.println("Table 'work_orders' dropped successfully.");
+        } catch (SQLException ex) {
+            System.err.println("Error dropping table 'work_orders': " + ex.getMessage());
+        }
+    }
+
     public boolean createWorkOrder(WorkOrder workOrder) throws SQLException {
-        String sqlNewWorkOrder = "INSERT INTO work_orders (order_owner_id, customer_id, order_date, delivery_date, shipping_address, status, subtotal) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        String sqlNewWorkOrder = "INSERT INTO work_orders (work_order_owner_id, customer_id, order_date, delivery_date, shipping_address, status, subtotal) VALUES (?, ?, ?, ?, ?, ?, ?)";
 
         try (PreparedStatement preparedStatement = connection.prepareStatement(sqlNewWorkOrder)) {
 
@@ -104,7 +115,7 @@ public class WorkOrdersDAOImplementation implements WorkOrdersDAO.WorkOrdersDAOI
 
     private WorkOrder mapToWorkOrder(ResultSet resultSet) throws SQLException {
         Integer workOrderId = resultSet.getInt("work_order_id");
-        Integer orderOwnerId = resultSet.getInt("order_owner_id"); // Adjust if needed
+        Integer orderOwnerId = resultSet.getInt("work_order_owner_id"); // Adjust if needed
         Integer customerId = resultSet.getInt("customer_id");
 
         ProductionUser orderOwner = productionUsers.get(orderOwnerId);
