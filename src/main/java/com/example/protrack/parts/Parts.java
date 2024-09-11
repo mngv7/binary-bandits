f64468a906f71f5d34bcb7859d49f71834fb22fe
Cheyne <cheyney99@gmail.com>
2024-09-11 16:58:20 +1000

    Adjusting all 'Parts' functionalities to match database tables

--- src/main/java/com/example/protrack/parts/Parts.java
+++ src/main/java/com/example/protrack/parts/Parts.java
@@ -5,15 +5,13 @@ public class Parts {
     private Integer partsId;
     private String name;
     private String description;
-    private String type;
     private Integer supplierId;
     private Double cost;
 
-    public Parts(Integer partsId, String name, String description, String type, Integer supplierId, Double cost) {
+    public Parts(Integer partsId, String name, String description, Integer supplierId, Double cost) {
         this.partsId = partsId;
         this.name = name;
         this.description = description;
-        this.type = type;
         this.supplierId = supplierId;
         this.cost = cost;
     }
@@ -28,10 +26,6 @@ public class Parts {
 
     public String getDescription() {
         return description;
-    }
-
-    public String getType() {
-        return type;
     }
 
     public Integer getSupplierId() {
