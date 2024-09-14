f8910ddbfd9d466201a0ff87d26b1029b1824a78
mngv7 <zackariya.taylor@gmail.com>
2024-09-14 10:34:41 +1000

    Added a button to confirm cancellation of product creation and test record creation.

--- src/main/java/com/example/protrack/applicationpages/CreateProductController.java
+++ src/main/java/com/example/protrack/applicationpages/CreateProductController.java
@@ -12,12 +12,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.sql.*;
@@ -252,7 +251,36 @@ public class CreateProductController {
 
     @FXML
     protected void onClosePopupButton() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.initStyle(StageStyle.UNDECORATED);
+        alert.setHeaderText("Cancel Product Creation");
+        alert.setContentText("Are you sure you want to cancel?");
+        alert.setGraphic(null);
+
+        DialogPane dialogPane = alert.getDialogPane();
+        String stylesheet = Objects.requireNonNull(Main.class.getResource("cancelAlert.css")).toExternalForm();
+        dialogPane.getStyleClass().add("cancelDialog");
+        dialogPane.getStylesheets().add(stylesheet);
+
+        ButtonType confirmBtn = new ButtonType("Confirm", ButtonBar.ButtonData.YES);
+        ButtonType backBtn = new ButtonType("Back", ButtonBar.ButtonData.NO);
+
+
+
+        alert.getButtonTypes().setAll(confirmBtn, backBtn);
         Stage stage = (Stage) closePopupButton.getScene().getWindow();
-        stage.close();
+        Node confirmButton = dialogPane.lookupButton(confirmBtn);
+        ButtonBar.setButtonData(confirmButton, ButtonBar.ButtonData.LEFT);
+        confirmButton.setId("confirmBtn");
+        Node backButton = dialogPane.lookupButton(backBtn);
+        ButtonBar.setButtonData(backButton, ButtonBar.ButtonData.RIGHT);
+        backButton.setId("backBtn");
+        alert.showAndWait();
+        if (alert.getResult().getButtonData() == ButtonBar.ButtonData.YES) {
+            alert.close();
+            stage.close();
+        } else if (alert.getResult().getButtonData() == ButtonBar.ButtonData.NO) {
+            alert.close();
+        }
     }
 }
