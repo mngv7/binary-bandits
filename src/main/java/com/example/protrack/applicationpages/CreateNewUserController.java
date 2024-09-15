8a82c204c4e73767da60abd4b5fcf6c2c61d024a
mngv7 <zackariya.taylor@gmail.com>
2024-09-15 13:30:30 +1000

    Added a button that links to a popup for creating a new user.

---
+++ src/main/java/com/example/protrack/applicationpages/CreateNewUserController.java
@@ -0,0 +1,50 @@
+package com.example.protrack.applicationpages;
+import javafx.fxml.FXML;
+
+import com.example.protrack.Main;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.util.Objects;
+
+public class CreateNewUserController {
+
+    @FXML
+    public Button closePopupButton;
+
+    public void onClosePopupButton() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.initStyle(StageStyle.UNDECORATED);
+        alert.setHeaderText("Cancel User Creation");
+        alert.setContentText("Are you sure you want to cancel?");
+        alert.setGraphic(null);
+
+        DialogPane dialogPane = alert.getDialogPane();
+        String stylesheet = Objects.requireNonNull(Main.class.getResource("cancelAlert.css")).toExternalForm();
+        dialogPane.getStyleClass().add("cancelDialog");
+        dialogPane.getStylesheets().add(stylesheet);
+
+        ButtonType confirmBtn = new ButtonType("Confirm", ButtonBar.ButtonData.YES);
+        ButtonType backBtn = new ButtonType("Back", ButtonBar.ButtonData.NO);
+
+
+
+        alert.getButtonTypes().setAll(confirmBtn, backBtn);
+        Stage stage = (Stage) closePopupButton.getScene().getWindow();
+        Node confirmButton = dialogPane.lookupButton(confirmBtn);
+        ButtonBar.setButtonData(confirmButton, ButtonBar.ButtonData.LEFT);
+        confirmButton.setId("confirmBtn");
+        Node backButton = dialogPane.lookupButton(backBtn);
+        ButtonBar.setButtonData(backButton, ButtonBar.ButtonData.RIGHT);
+        backButton.setId("backBtn");
+        alert.showAndWait();
+        if (alert.getResult().getButtonData() == ButtonBar.ButtonData.YES) {
+            alert.close();
+            stage.close();
+        } else if (alert.getResult().getButtonData() == ButtonBar.ButtonData.NO) {
+            alert.close();
+        }
+    }
+}
