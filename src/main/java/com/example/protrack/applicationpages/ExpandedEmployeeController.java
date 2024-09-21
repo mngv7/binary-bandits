cdef1636fe64b1eb9db3ceeb0d594649e02bdc9f
mngv7 <zackariya.taylor@gmail.com>
2024-09-21 16:32:38 +1000

    Employees name links to a their corresponding expanded page view.

--- src/main/java/com/example/protrack/applicationpages/ExpandedEmployeeController.java
+++ src/main/java/com/example/protrack/applicationpages/ExpandedEmployeeController.java
@@ -1,4 +1,33 @@
 package com.example.protrack.applicationpages;
 
+import com.example.protrack.employees.SelectedEmployeeSingleton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ExpandedEmployeeController {
+
+    @FXML
+    public Button removeEmployeeButton;
+
+    @FXML
+    private Label employeeNameTitle;
+
+    public void initialize() {
+        String firstName = SelectedEmployeeSingleton.getInstance().getEmployeeFirstName();
+        String lastname = SelectedEmployeeSingleton.getInstance().getEmployeeLastName();
+        employeeNameTitle.setText(firstName + " " + lastname);
+    }
+
+    // Method to go back to the Employees view
+    public void backToEmployees() {
+
+    }
 }
