ce09c2cb65386030b41b236c4d469c203e5538de
seahauk <n11945427@qut.edu.au>
2024-10-01 13:04:19 +1000

    Initial abstraction of functions completed, to complete setup another controller for handling of edit and delete actions

--- src/main/java/com/example/protrack/customer/CustomerController.java
+++ src/main/java/com/example/protrack/customer/CustomerController.java
@@ -9,20 +9,7 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import java.util.List;
 
 public class CustomerController {
-    @FXML
-    private TextField firstNameField;
-    @FXML
-    private TextField lastNameField;
-    @FXML
-    private TextField emailField;
-    @FXML
-    private TextField phoneNumberField;
-    @FXML
-    private TextField billingAddressField;
-    @FXML
-    private TextField shippingAddressField;
-    @FXML
-    private ComboBox<String> statusComboBox;
+
     @FXML
     private TableView<Customer> customersTableView;
     @FXML
@@ -72,44 +59,5 @@ public class CustomerController {
         tableCustomers.clear();
         tableCustomers.addAll(customers);
         customersTableView.setItems(tableCustomers);
-    }
-
-    @FXML
-    private void handleAddCustomer() {
-        // Gather data from fields
-        String firstName = firstNameField.getText();
-        String lastName = lastNameField.getText();
-        String email = emailField.getText();
-        String phoneNumber = phoneNumberField.getText();
-        String billingAddress = billingAddressField.getText();
-        String shippingAddress = shippingAddressField.getText();
-        String status = statusComboBox.getValue();
-
-        // Create a new customer object and add to the database
-        Customer newCustomer = new Customer(null, firstName, lastName, email, phoneNumber, billingAddress, shippingAddress, status);
-        if (customerDAO.addCustomer(newCustomer)) {
-            loadCustomers(); // Reload customers to update the table
-            clearFields(); // Clear input fields after adding
-        }
-    }
-
-    @FXML
-    private void handleUpdateCustomer() {
-        // Implementation for updating a selected customer
-    }
-
-    @FXML
-    private void handleDeleteCustomer() {
-        // Implementation for deleting a selected customer
-    }
-
-    private void clearFields() {
-        firstNameField.clear();
-        lastNameField.clear();
-        emailField.clear();
-        phoneNumberField.clear();
-        billingAddressField.clear();
-        shippingAddressField.clear();
-        statusComboBox.setValue(null);
     }
 }
--- src/main/java/com/example/protrack/customer/EditCustomerController.java
+++ src/main/java/com/example/protrack/customer/EditCustomerController.java
@@ -1,4 +1,81 @@
 package com.example.protrack.customer;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
+import java.awt.*;
+
 public class EditCustomerController {
+
+    /*
+    @FXML
+    private TextField firstNameField;
+
+    @FXML
+    private TextField lastNameField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField phoneNumberField;
+
+    @FXML
+    private TextField billingAddressField;
+
+    @FXML
+    private TextField shippingAddressField;
+
+    @FXML
+    private ComboBox<String> statusCombo;
+
+    private CustomerDAO customerDAO;
+
+    public EditCustomerController() {
+        customerDAO = new CustomerDAO();
+    }
+
+    @FXML
+    private void addCustomer() {
+        // Gather data from fields
+        String firstName = firstNameField.getText();
+        String lastName = lastNameField.getText();
+        String email = emailField.getText();
+        String phoneNumber = phoneNumberField.getText();
+        String billingAddress = billingAddressField.getText();
+        String shippingAddress = shippingAddressField.getText();
+        String status = statusCombo.getValue();
+
+        // Create a new customer object and add to the database
+        Customer customer = new Customer(null, firstName, lastName, email, phoneNumber, billingAddress, shippingAddress, status);
+        if (customerDAO.addCustomer(customer)) {
+            clearFields(); // Clear input fields after adding
+        }
+    }
+
+    @FXML
+    private void updateCustomer() {
+        // Implementation for updating a selected customer
+    }
+
+    @FXML
+    private void deleteCustomer() {
+        // Implementation for deleting a selected customer
+    }
+
+    private void clearFields() {
+        firstNameField.setText("");
+        lastNameField.setText("");
+        emailField.setText("");
+        phoneNumberField.setText("");
+        billingAddressField.setText("");
+        shippingAddressField.setText("");
+        statusCombo.setValue(null);
+    }
+
+     */
+
+
 }
