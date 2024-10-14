d4faacc8e5afee7ec71bb83bc64745152b2250d1
Jean-Luc Mackail <fuzzyquils@scrapyard.link>
2024-10-14 21:54:58 +1000

    Javadoc warehouse and workstation interfaces.

--- src/main/java/com/example/protrack/warehouseutil/Warehouse.java
+++ src/main/java/com/example/protrack/warehouseutil/Warehouse.java
@@ -1,12 +1,42 @@
 package com.example.protrack.warehouseutil;
 
 public interface Warehouse {
-    /* Getters and setters. Self-explanatory. */
+    /**
+     * Gets the location ID of a warehouse.
+     * @return the location ID as an integer.
+     */
     int getWarehouseLocationId();
+
+    /**
+     * Set the location ID of a warehouse to a specific value.
+     * NOTE: In ProTrack, there is only one warehouse, therefore this
+     *       value is usually always 0.
+     * @param warehouseId the location ID to set this warehouse to.
+     */
     void setWarehouseLocationId(int warehouseId);
+
+    /**
+     * Gets the location alias of the warehouse.
+     * @return A string containing the location alias.
+     */
     String getWarehouseLocationAlias();
+
+    /**
+     * Sets the location alias of the warehouse.
+     * @param warehouseName the new location alias to set on the warehouse.
+     */
     void setWarehouseLocationAlias(String warehouseName);
+
+    /**
+     * Gets the capacity of a warehouse.
+     * @return An integer describing the maximum number of parts a warehouse can hold.
+     */
     int getWarehouseMaxParts();
+
+    /**
+     * Sets the capacity of a warehouse.
+     * @param maxParts = The new capacity for the warehouse.
+     */
     void setWarehouseMaxParts(int maxParts);
 
     /* TODO: See if these are needed anymore. */
@@ -20,6 +50,28 @@ public interface Warehouse {
      * These functions generally need a DAO variable, however some implementations leave
      * it unused. Such cases are explicitly labeled in their implementation's code comments.
      */
+
+    /**
+     * Adds a specific quantity of the given parts ID to a warehouse.
+     * Such part ID numbers ideally should be cross-referenced with their corresponding records
+     * retrieved from the Parts DAO.
+     * @param dao The LocationsAndContentsDAO instance to utilise for database access.
+     *            Mock implementations may choose to ignore this argument.
+     * @param partsId The ID of the part to add to the warehouse.
+     * @param quantity The quantity of the part to add to the warehouse.
+     */
     void addPartsIdWithQuantity (LocationsAndContentsDAO dao, int partsId, int quantity);
+
+    /**
+     * Removes a specific quantity of the given parts ID to a warehouse, with a quantity higher than
+     * the remaining quantity of the given parts ID resulting in removal of the partsID record from the warehouse.
+     * Such part ID numbers ideally should be cross-referenced with their corresponding records
+     * retrieved from the Parts DAO.
+     * @param dao The LocationsAndContentsDAO instance to utilise for database access.
+     *            Mock implementations may choose to ignore this argument.
+     * @param partsId The ID of the part to remove from the warehouse.
+     * @param quantity The quantity of the part to remove from the warehouse.
+     *                 The behaviour in the case of a quantity higher than what's remaining is implementation defined.
+     */
     void removePartsIdWithQuantity (LocationsAndContentsDAO dao, int partsId, int quantity);
 }
--- src/main/java/com/example/protrack/warehouseutil/Workstation.java
+++ src/main/java/com/example/protrack/warehouseutil/Workstation.java
@@ -1,11 +1,40 @@
 package com.example.protrack.warehouseutil;
 
 public interface Workstation {
+    /**
+     * Gets the workstation's location ID.
+     * @return An integer describing the workstation's location ID
+     */
     int getWorkstationLocationId();
+
+    /**
+     * Sets the workstation's location ID.
+     * @param workstationID The new location ID to assign to the workstation.
+     */
     void setWorkstationLocationId(int workstationID);
+
+    /**
+     * Gets the workstation's location alias.
+     * @return A string containing this workstation's location alias.
+     */
     String getWorkstationLocationAlias();
+
+    /**
+     * Sets the workstation's location alias to a new name.
+     * @param workstationName The new name to set the workstation to.
+     */
     void setWorkstationLocationAlias(String workstationName);
+
+    /**
+     * Gets the capacity of the workstation.
+     * @return An integer describing the capacity of the workstation.
+     */
     int getWorkstationMaxParts();
+
+    /**
+     * Sets a new capacity for the workstation.
+     * @param maxParts The new capacity of the workstation expressed as an integer.
+     */
     void setWorkstationMaxParts(int maxParts);
 
     /* TODO: See if these are actually needed anymore. */
@@ -20,7 +49,34 @@ public interface Workstation {
      * These functions generally need a DAO variable, however some implementations leave
      * it unused. Such cases are explicitly labeled in their implementation's code comments.
      */
+
+    /**
+     * Imports the given partsID of the given quantity from the target warehouse.
+     * Implementations of this interface are recommended to also subtract said quantity from the warehouse itself.
+     * @param targetWarehouse The target instance of the Warehouse interface to subtract parts from.
+     * @param dao The LocationsAndContentsDAO to update. Mock implementations can ignore this argument.
+     * @param componentId The given parts ID to import from the warehouse.
+     * @param quantity The quantity of the given parts ID to import from the warehouse.
+     */
     void importPartsIdWithQuantityFromWarehouse(Warehouse targetWarehouse, LocationsAndContentsDAO dao, int componentId, int quantity);
+
+    /**
+     * Returns the given partsID of the given quantity to the target warehouse.
+     * Implementations of this interface are recommended to also add said quantity to the warehouse itself.
+     * @param targetWarehouse The target instance of the Warehouse interface to add parts back to.
+     * @param dao The LocationsAndContentsDAO to update. Mock implementations can ignore this argument.
+     * @param componentId The given parts ID to return to the warehouse.
+     * @param quantity The quantity of the given parts ID to return to the warehouse.
+     */
     void returnPartsIdWithQuantityToWarehouse(Warehouse targetWarehouse, LocationsAndContentsDAO dao, int componentId, int quantity);
+
+    /**
+     * Returns ALL parts currently allocated to the workstation to the target warehouse.
+     * Implementations of this interface are required to add ALL parts back to the target warehouse unless targetWarehouse is null.
+     * <p>NOTE: This function is intended to be called when destroying a workstation interface instance; there should not
+     * be a need to call this function otherwise!</p>
+     * @param targetWarehouse The target instance of the Warehouse interface to add all parts back to.
+     * @param dao The LocationsAndContentsDAO to update. Mock implementations can ignore this argument.
+     */
     void returnAllPartsToWarehouse (Warehouse targetWarehouse, LocationsAndContentsDAO dao);
 }
